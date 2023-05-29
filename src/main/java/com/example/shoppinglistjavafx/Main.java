a8c8f223dfe1db4049a99a211f4e3b487da8f583
olasgolas11 <olekgolus02@gmail.com>
2023-05-29 13:17:48 +0200

    Init controller

--- src/main/java/com/example/shoppinglistjavafx/Main.java
+++ src/main/java/com/example/shoppinglistjavafx/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader shoppingListView = new FXMLLoader(Main.class.getResource("shopping-list-view.fxml"));
-        Scene shoppingListScene = new Scene(shoppingListView.load(), 320, 240);
+        Scene shoppingListScene = new Scene(shoppingListView.load(), 800, 600);
         stage.setTitle("Shopping list");
         stage.setScene(shoppingListScene);
         stage.show();
--- src/main/java/com/example/shoppinglistjavafx/ShoppingListController.java
+++ src/main/java/com/example/shoppinglistjavafx/ShoppingListController.java
@@ -5,10 +5,46 @@ import javafx.scene.control.Label;
 
 public class ShoppingListController {
     @FXML
-    private Label welcomeText;
+    private Label selectedOptionText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onAddProductButtonClick() {
+        selectedOptionText.setText("Add product");
+    }
+
+    @FXML
+    protected void onDisplayAllProductsButtonClick() {
+        selectedOptionText.setText("Display all added products");
+    }
+
+    @FXML
+    protected void onDisplayAllProductsFromCategoryButtonClick() {
+        selectedOptionText.setText("Display all products from category");
+    }
+
+    @FXML
+    protected void onResetShoppingListButtonClick() {
+        selectedOptionText.setText("Reset shopping list");
+    }
+
+    @FXML
+    protected void onDeleteAllProductsFromCategoryButtonClick() {
+        selectedOptionText.setText("Delete all products from category");
+    }
+
+    @FXML
+    protected void onDeleteProductFromCategoryButtonClick() {
+        selectedOptionText.setText("Delete product from category");
+    }
+
+    @FXML
+    protected void onSaveListButtonClick() {
+        selectedOptionText.setText("Save list");
+    }
+
+    @FXML
+    protected void onExitButtonClick() {
+        selectedOptionText.setText("Exit");
     }
 }
